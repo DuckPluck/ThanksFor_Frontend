60c2ad93dc7664aa97c62b535785d98279b3e506
DuckPluck <w.utkindaniil@gmail.com>
2022-08-08 19:38:29 +0300

    Account making server interaction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account/account.component';
-import { MasonryTestComponent } from './masonry-test/masonry-test.component';
 import { NgxMasonryModule } from 'ngx-masonry';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MasontyDemoComponent } from './masonty-demo/masonty-demo.component';
@@ -14,7 +13,6 @@ import { MasontyDemoComponent } from './masonty-demo/masonty-demo.component';
   declarations: [
     AppComponent,
     AccountComponent,
-    MasonryTestComponent,
     MasontyDemoComponent
   ],
   imports: [
